aed96afe555e92afa838248b5e82452c588308e5
gaming-online <saintfire15@proton.me>
2026-03-10 20:54:48 +0100

    Create stealth.js

---
+++ assets/js/stealth.js
@@ -0,0 +1,18 @@
+document.addEventListener('keydown', (e) => {
+    // Shift + P: Redirect to Google Docs
+    if (e.shiftKey && e.key === 'P') {
+        window.location.href = "https://docs.google.com/";
+    }
+    
+    // Shift + L: Disguise the tab
+    if (e.shiftKey && e.key === 'L') {
+        document.title = "Google Documents";
+        let link = document.querySelector("link[rel~='icon']");
+        if (!link) {
+            link = document.createElement('link');
+            link.rel = 'icon';
+            document.head.appendChild(link);
+        }
+        link.href = "https://ssl.gstatic.com/docs/documents/images/kix-favicon7.ico";
+    }
+});
