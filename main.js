75e70aeacbe4175052686dbaece23d912f83f51c
gaming-online <saintfire15@proton.me>
2026-03-10 21:01:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 document.addEventListener('keydown', (e) => {
     // Shift + P: Redirect to Google Docs
-    if (e.shiftKey && e.key === 'P') {
+    if (e.shiftKey && e.code === 'KeyP') {
         window.location.href = "https://docs.google.com/";
     }
 
-    // Shift + L: Change Title and Favicon
-    if (e.shiftKey && e.key === 'L') {
-        document.title = "Google Documents";
-        let link = document.querySelector("link[rel~='icon']");
-        if (!link) {
-            link = document.createElement('link');
-            link.rel = 'icon';
-            document.head.appendChild(link);
-        }
-        link.href = "https://ssl.gstatic.com/docs/documents/images/kix-favicon7.ico";
-        console.log("Cloak mode activated.");
+    // Shift + L: Tab Cloaking
+    if (e.shiftKey && e.code === 'KeyL') {
+        document.title = "Google Docs";
+        let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+        link.type = 'image/x-icon';
+        link.rel = 'shortcut icon';
+        link.href = 'https://ssl.gstatic.com/docs/documents/images/kix-favicon7.ico';
+        document.getElementsByTagName('head')[0].appendChild(link);
     }
 });
